1f0520901b0dfa610114725e65fdbbd0c0bd2fed
Nick Kuznetsov <nich-chel@yandex.ru>
2020-11-26 17:54:34 +0300

    button with css variables

---
+++ src/Button/Button.tsx
@@ -0,0 +1,23 @@
+import React, { FC } from 'react';
+
+import cn from 'classnames';
+import { Variant } from './constants';
+
+interface IButtonProps {
+  className?: string;
+  variant?: Variant;
+}
+
+const Button: FC<IButtonProps> = ({
+  children,
+  className,
+  variant = Variant.Default,
+}) => {
+  return (
+    <button className={cn('Button', `Button--${variant}`, className)}>
+      {children}
+    </button>
+  );
+};
+
+export default Button;
---
+++ stories/Button.stories.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import {Meta} from '@storybook/react';
+import Button, {Variant} from '../src/Button';
+
+import '../src/Button/Button.css';
+
+export const ButtonStory = () => <Button>Button</Button>;
+
+export const ButtonPrimary = () => <Button variant={Variant.Primary}>Primary Button</Button>;
+
+export const ButtonDanger = () => <Button variant={Variant.Danger}>Danger Button</Button>;
+
+ButtonStory.storyName  = 'Default';
+ButtonPrimary.storyName  = 'Primary';
+ButtonDanger.storyName  = 'Danger';
+
+const meta: Meta = {
+  title: 'Button',
+  component: Button,
+};
+
+export default meta;
