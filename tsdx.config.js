e2d1364af87335b5b1b4a1855bcebfb89c2e4dbc
Nick Kuznetsov <nich-chel@yandex.ru>
2020-11-27 14:42:05 +0300

    CSS copy without rollup-plugin-copy

--- tsdx.config.js
+++ tsdx.config.js
@@ -2,13 +2,13 @@ const copy = require('rollup-plugin-copy');
 
 module.exports = {
   rollup(config) {
-    config.plugins.push(
-      copy({
-        targets: [{ src: 'src/**/*.css', dest: 'dist' }],
-        flatten: false,
-      })
-    );
-
+    // config.plugins.unshift(
+    //   copy({
+    //     targets: [{ src: 'src/**/*.css', dest: 'dist' }],
+    //     flatten: false,
+    //   })
+    // );
+    // console.log(config.plugins);
     return config;
   },
 };
