e3b2bfbbf3fafc29b5486d59be3440466ffc49c1
Nick Kuznetsov <nich-chel@yandex.ru>
2020-12-25 17:47:40 +0300

    Content component

--- src/Content/Content.tsx
+++ src/Content/Content.tsx
@@ -1,15 +1,24 @@
 // @ts-ignore
 import React, { FC, ReactNode, ReactElement } from 'react';
+import cn from 'classnames';
 import renderAsProp from '../utils/renderAsProp';
+import { TSize } from '../constansts';
 
 interface IContentProps {
     as?: ReactNode | ReactElement | string;
+    maxWidth?: TSize;
+    auto?: boolean;
+    className?: string;
 }
 
 const Content: FC<IContentProps> = props => {
-    const { children, as = 'div', ...rest } = props;
+    const { children, as = 'section', maxWidth = 'Medium', className, auto, ...rest } = props;
 
-    return renderAsProp({ as, props: rest, children });
+    return renderAsProp({
+        as,
+        props: { className: cn(className, 'Content', `maxWidth${maxWidth}`, auto && 'ContentCenter'), ...rest },
+        children
+    });
 };
 
 export default Content;
