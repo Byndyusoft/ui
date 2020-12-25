cdef13e494b25f9ce53edba967ed167bc1764bbd
Nick Kuznetsov <nich-chel@yandex.ru>
2020-12-25 17:51:47 +0300

    index.ts imports

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,7 @@ export { default as useInterval } from './hooks/useInterval';
 export { default as useLatestRef } from './hooks/useLatestRef';
 export { default as useTimeout } from './hooks/useTimeout';
 export { default as Button } from './Button';
+export { default as Skeleton } from './Skeleton';
+export { default as Content } from './Content';
 export { Variant as ButtonVariant } from './Button';
 export * from './Button/constants';
