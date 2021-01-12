32a4ebdce3d45de26e02026f951e4f3aadf8441d
ancientbag <pleasenonaga@gmail.com>
2021-01-12 16:17:51 +0500

    Image stories

---
+++ src/components/Image/Image.stories.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import { Meta } from '@storybook/react';
+import Image from '.';
+
+export const DefaultImageStory = () => {
+    return (
+        <Image
+            width={480}
+            height={360}
+            src="https://images.pexels.com/photos/531321/pexels-photo-531321.jpeg?crop=entropy&cs=srgb&dl=pexels-pixabay-531321.jpg&fit=crop&fm=jpg&h=3313&w=4970"
+        />
+    );
+};
+
+export const BackgroundPlaceholderColor = () => {
+    return (
+        <Image
+            bgPlaceholderColor="#3e3e3e"
+            width={480}
+            height={360}
+            src="https://images.pexels.com/photos/531321/pexels-photo-531321.jpeg?crop=entropy&cs=srgb&dl=pexels-pixabay-531321.jpg&fit=crop&fm=jpg&h=3313&w=4970"
+        />
+    );
+};
+
+export const BackgroundPlaceholderImage = () => {
+    return (
+        <Image
+            bgPlaceholderImage="https://miro.medium.com/max/500/0*-ouKIOsDCzVCTjK-.png"
+            width={480}
+            height={360}
+            src="https://images.pexels.com/photos/531321/pexels-photo-531321.jpeg?crop=entropy&cs=srgb&dl=pexels-pixabay-531321.jpg&fit=crop&fm=jpg&h=3313&w=4970"
+        />
+    );
+};
+
+DefaultImageStory.storyName = 'Default';
+BackgroundPlaceholderColor.storyName = 'Background placeholder color';
+BackgroundPlaceholderImage.storyName = 'Background placeholder image';
+
+const meta: Meta = {
+    title: 'Components/Image',
+    component: Image
+};
+
+export default meta;
