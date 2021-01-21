91e0d933dc779ca1ef465f84b3e5ec818f724aa2
Nick Kuznetsov <nich-chel@yandex.ru>
2021-01-21 13:02:39 +0300

    useCountdown refactor

--- src/hooks/useCountdown.stories.tsx
+++ src/hooks/useCountdown.stories.tsx
@@ -1,15 +1,12 @@
 import React from 'react';
 import { Meta, Story } from '@storybook/react';
-import useCountdown from "./useCountdown";
+import useCountdown from './useCountdown';
 
 export const useCountdownStory: Story = () => {
-    const count = useCountdown({count: 5});
+    const { count } = useCountdown({ count: 5 });
 
-    return <div>
-        {count ? count : 'отсчет закончен'}
-    </div>
-}
-
+    return <div>{count ? count : 'отсчет закончен'}</div>;
+};
 
 useCountdownStory.storyName = 'useCountdown';
 
--- src/hooks/useCountdown.ts
+++ src/hooks/useCountdown.ts
@@ -1,23 +1,38 @@
-import { useEffect, useState } from 'react';
+import { useState, useEffect } from 'react';
+import useInterval from './useInterval';
 
 interface ICountdown {
     count: number;
     onStop?: () => void;
 }
 
-function useCountdown({ count, onStop }: ICountdown): number {
+function useCountdown({ count, onStop }: ICountdown): { count: number; stop: () => void; start: () => void } {
     const [counter, setCounter] = useState(count);
 
-    useEffect(() => {
-        if (counter === 0 && onStop) {
-            onStop();
-        }
+    const { start: startIterval, stop } = useInterval(() => {
         if (counter > 0) {
-            setTimeout(() => setCounter(counter - 1), 1000);
+            setCounter(prev => prev - 1);
         }
-    }, [counter]);
+    });
 
-    return counter;
+    useEffect(() => {
+        startIterval(1000);
+    }, [startIterval]);
+
+    if (count === 0) {
+        stop();
+    }
+
+    if (count === 0 && onStop) {
+        onStop();
+    }
+
+    const reset = () => {
+        setCounter(count);
+        startIterval(1000);
+    };
+
+    return { count: counter, stop, start: reset };
 }
 
 export default useCountdown;
