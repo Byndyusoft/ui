a5850d19248ad3d6fbea3f7dbe239fc904889bda
ancientbag <pleasenonaga@gmail.com>
2021-01-22 16:09:52 +0500

    useFocus hook added

---
+++ src/hooks/useFocus/useFocus.ts
@@ -0,0 +1,29 @@
+import { useRef, useEffect, useCallback } from 'react';
+
+type TUseFocusResponse = (e: HTMLElement | null) => void;
+
+const useFocus = (): TUseFocusResponse => {
+    const ref = useRef<HTMLElement | null>(null);
+
+    const bindRefHandler = useCallback(
+        (e: HTMLElement | null) => {
+            // Remove listener when changing ref
+            if (e && ref.current && e !== ref.current) {
+                console.log('focusing');
+                ref.current.focus();
+            }
+            ref.current = e;
+        },
+        [ref]
+    );
+
+    useEffect(() => {
+        if (ref.current) {
+            ref.current.focus();
+        }
+    }, [ref]);
+
+    return bindRefHandler;
+};
+
+export default useFocus;
