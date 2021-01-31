534758dfeecc484d0dc451c63c6312618b49b4be
Eugene Abrosimov <sadcitizen@yandex.ru>
2021-01-31 16:06:02 +0500

    add supporting styles and files to jest

--- jest.config.js
+++ jest.config.js
@@ -6,5 +6,9 @@ module.exports = {
     },
     testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
     moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+    moduleNameMapper: {
+        '\\.css$': 'identity-obj-proxy',
+        '\\.(ico|jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga|pdf)$':'<rootDir>/.jest/file.ts'
+    },
     coveragePathIgnorePatterns: ['index.ts']
 };
