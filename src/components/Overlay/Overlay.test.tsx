e591db9c80692b33f3fcd38f909ca7d2fa0a8fcf
Eugene Abrosimov <sadcitizen@yandex.ru>
2021-01-31 16:06:16 +0500

    add test

---
+++ src/components/Overlay/Overlay.test.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import Overlay from './Overlay';
+
+describe('components/Overlay', () => {
+    test('renders correctly with children', () => {
+        render(<Overlay isOpen><div data-testid="content" /></Overlay>);
+
+        expect(screen.getByTestId('content')).toBeInTheDocument();
+    });
+});
