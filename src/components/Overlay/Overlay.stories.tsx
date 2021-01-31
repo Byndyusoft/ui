4d0576b601e5790053c3237d3eaee421e6d49c86
Eugene Abrosimov <sadcitizen@yandex.ru>
2021-01-31 15:22:14 +0500

    add stories for Overlay

--- src/components/Overlay/Overlay.stories.tsx
+++ src/components/Overlay/Overlay.stories.tsx
@@ -2,10 +2,19 @@ import React from 'react';
 import { Meta, Story } from '@storybook/react';
 import Overlay from './Overlay';
 import './Overlay.css';
+import './Overlay.stories.css';
 
-export const DefaultStory: Story = () => <Overlay>Содержимое оверлея</Overlay>;
+export const DefaultStory: Story = () => <Overlay isOpen>Цвет фона: rgba(0, 0, 0, 0.5)</Overlay>;
 
 DefaultStory.storyName = 'Default';
+
+export const CustomBackgroundColorStory: Story = () => (
+    <div className="OverlayCustomBackgroundColorStory">
+        <Overlay isOpen>Цвет фона: rgba(30, 30, 130, 0.3)</Overlay>
+    </div>
+);
+
+CustomBackgroundColorStory.storyName = 'Custom background color';
 
 const meta: Meta = {
     title: 'components/Work in progress/Overlay',
