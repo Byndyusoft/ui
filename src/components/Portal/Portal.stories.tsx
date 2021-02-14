f70deaa61faa34d1fd02f24686401bcbe2953183
Eugene Abrosimov <sadcitizen@yandex.ru>
2021-02-14 15:22:33 +0500

    update portal component

--- src/components/Portal/Portal.stories.tsx
+++ src/components/Portal/Portal.stories.tsx
@@ -1,27 +1,41 @@
-import React, { CSSProperties } from 'react';
+import React from 'react';
 import { Meta, Story } from '@storybook/react';
+import cn from 'classnames';
 import Portal from './Portal';
+import './Portal.stories.css';
 
 export const DefaultStory: Story = () => {
-    const style: CSSProperties = {
-        position: 'absolute',
-        top: '0',
-        right: '0',
-        backgroundColor: 'lavender',
-        padding: '1rem 2rem'
-    };
-
     return (
-        <Portal>
-            <div style={style}>Текст в портале</div>
-        </Portal>
+        <div className="PortalStory">
+            <p>Тест в основном контейнере приложения</p>
+            <Portal id="topRightPortal">
+                <div className={cn('PortalStory__Block', 'PortalStory__Block--Top', 'PortalStory__Block--Right')}>
+                    Текст в портале
+                </div>
+            </Portal>
+            <Portal id="bottomRightPortal">
+                <div className={cn('PortalStory__Block', 'PortalStory__Block--Bottom', 'PortalStory__Block--Right')}>
+                    Текст в портале
+                </div>
+            </Portal>
+            <Portal id="bottomLightPortal">
+                <div className={cn('PortalStory__Block', 'PortalStory__Block--Bottom', 'PortalStory__Block--Left')}>
+                    Текст в портале
+                </div>
+            </Portal>
+            <Portal id="topLeftPortal">
+                <div className={cn('PortalStory__Block', 'PortalStory__Block--Top', 'PortalStory__Block--Left')}>
+                    Текст в портале
+                </div>
+            </Portal>
+        </div>
     );
 };
 
 DefaultStory.storyName = 'Default';
 
 const meta: Meta = {
-    title: 'components/Work in progress/Portal',
+    title: 'components/Portal',
     component: Portal
 };
 
--- src/components/Portal/Portal.tsx
+++ src/components/Portal/Portal.tsx
@@ -1,20 +1,30 @@
-import React, { FC, useEffect } from 'react';
+import { FC, useEffect, useState } from 'react';
 import { createPortal } from 'react-dom';
 
-export interface IPortalProps {}
+export interface IPortalProps {
+    id?: string
+}
 
-const Portal: FC<IPortalProps> = ({ children }) => {
-    const container = React.useRef<HTMLDivElement>(document.createElement('div'));
+const Portal: FC<IPortalProps> = ({ children, id }) => {
+    const [container] = useState<HTMLDivElement>(() => {
+        const element = document.createElement('div');
+
+        if (id && id.length > 0) {
+            element.setAttribute('id', id);
+        }
+
+        return element;
+    });
 
     useEffect(() => {
-        document.body.appendChild(container.current);
+        document.body.appendChild(container);
 
         return () => {
-            document.body.removeChild(container.current);
+            document.body.removeChild(container);
         };
     }, []);
 
-    return createPortal(children, container.current);
+    return createPortal(children, container);
 };
 
 export default Portal;
