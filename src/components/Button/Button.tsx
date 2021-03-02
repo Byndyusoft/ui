236c3df3d687e63d233d07471e46edcb5a08acf2
ancientbag <pleasenonaga@gmail.com>
2021-03-02 15:03:49 +0500

    button type return, cursor for actionable buttons

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,12 +1,13 @@
 import React, { FC } from 'react';
 import cn from 'classnames';
-import { Variant, Size } from './constants';
+import { Variant, Size, Type } from './constants';
 import './Button.css';
 
 interface IButtonProps {
     className?: string;
     variant?: Variant;
     size?: Size;
+    type?: Type;
     isDisabled?: boolean;
     isPending?: boolean;
 }
@@ -14,23 +15,25 @@ interface IButtonProps {
 const Button: FC<IButtonProps> = ({
     children,
     className,
-    size = Size.Medium,
     variant = Variant.Primary,
+    size = Size.Medium,
+    type = Type.Button,
     isDisabled = false,
     isPending = false
 }) => (
     <button
-        type="button"
+        // eslint-disable-next-line react/button-has-type
+        type={type}
         className={cn(
             'Button',
             `Button--${variant}`,
             `Button--${size}`,
-            !isDisabled && !isPending && `actionable`,
+            !isDisabled && !isPending && `Button--actionable`,
             isDisabled && `disabled`,
             className
         )}
     >
-        {isPending && `Загрузка`}
+        {isPending && `Загружаем...`}
         {!isPending && children}
     </button>
 );
