20c43f8c47031fe8a0cff01b941b95849d7a368a
ancientbag <pleasenonaga@gmail.com>
2021-03-04 16:35:04 +0500

    jest style and file mock

--- jest.config.js
+++ jest.config.js
@@ -6,5 +6,9 @@ module.exports = {
     },
     testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
     moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-    coveragePathIgnorePatterns: ['index.ts']
+    coveragePathIgnorePatterns: ['index.ts'],
+    moduleNameMapper: {
+        '\\.(css|less|sass|scss)$': '<rootDir>/.jest/styleMock.js',
+        '\\.(gif|ttf|eot|svg)$': '<rootDir>/.jest/fileMock.js'
+    }
 };
