469b33d72eef9b25bfc3cc9bb7b4888bb649b209
Eugene Abrosimov <sadcitizen@yandex.ru>
2021-09-19 21:42:36 +0500

    update storybook configuration

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,8 +1,4 @@
 module.exports = {
-    stories: ['../src/**/*.stories.@(ts|tsx|js|jsx)'],
-    addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
-    // https://storybook.js.org/docs/react/configure/typescript#mainjs-configuration
-    typescript: {
-        check: true // type-check stories during Storybook build
-    }
+    stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+    addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-a11y']
 };
