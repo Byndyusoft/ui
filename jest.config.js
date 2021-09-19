f4a8ac576931e2bd145bbdd71dd3180f223ed544
Eugene Abrosimov <sadcitizen@yandex.ru>
2021-09-19 22:33:19 +0500

    update testing setup

--- jest.config.js
+++ jest.config.js
@@ -4,7 +4,15 @@ module.exports = {
     transform: {
         '^.+\\.tsx?$': 'ts-jest'
     },
-    testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
-    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+    testMatch: ['**/*.tests.(ts|tsx)'],
+    testEnvironment: 'jsdom',
+    moduleDirectories: ['node_modules'],
+    moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx'],
+    moduleNameMapper: {
+        '\\.css$': 'identity-obj-proxy',
+        '\\.(ico|jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga|pdf)$':
+            '<rootDir>/.jest/file.ts'
+    },
+    coverageDirectory: '<rootDir>/.coverage',
     coveragePathIgnorePatterns: ['index.ts']
 };
