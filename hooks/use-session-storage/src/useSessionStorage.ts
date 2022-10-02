59ee663f1cbc0ca5467e791225ba43f4a43c6ca9
Eugene Abrosimov <sadcitizen@yandex.ru>
2022-10-02 14:29:58 +0500

    feat(use-session-storage): wip

---
+++ hooks/use-session-storage/src/useSessionStorage.ts
@@ -0,0 +1,7 @@
+import { SessionStorageService } from '@byndyusoft-ui/session-storage';
+
+export interface IUseSessionStorage {}
+
+export default function useSessionStorage<TValue>(): IUseSessionStorage {
+    return {};
+}
