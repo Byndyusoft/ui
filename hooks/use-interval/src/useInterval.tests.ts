4b14323d8672ccf001d299d149c95cfd01758d2f
Eugene Abrosimov <sadcitizen@yandex.ru>
2022-10-11 18:13:11 +0500

    feat(use-interval): add tests

--- hooks/use-interval/src/useInterval.tests.ts
+++ hooks/use-interval/src/useInterval.tests.ts
@@ -1,8 +1,59 @@
 import { renderHook } from '@testing-library/react-hooks';
 import useInterval from './useInterval';
 
+const setup = (callback: () => void, delay: number | null) => renderHook(() => useInterval(callback, delay));
+
 describe('hooks/useInterval', () => {
-    test('to be true', () => {
-        expect(true).toBeTruthy();
+    beforeAll(() => {
+        jest.useFakeTimers();
+        jest.spyOn(global, 'clearInterval');
+    });
+
+    test('calls callback', () => {
+        const callback = jest.fn();
+
+        setup(callback, 100);
+
+        expect(callback).not.toBeCalled();
+
+        jest.advanceTimersByTime(101);
+
+        expect(callback).toHaveBeenCalledTimes(1);
+
+        jest.advanceTimersByTime(100);
+
+        expect(callback).toHaveBeenCalledTimes(2);
+
+        jest.advanceTimersByTime(100);
+
+        expect(callback).toHaveBeenCalledTimes(3);
+    });
+
+    test('does not call callback if delay is null', () => {
+        const callback = jest.fn();
+
+        setup(callback, null);
+
+        expect(callback).not.toBeCalled();
+
+        jest.advanceTimersByTime(150);
+
+        expect(callback).not.toBeCalled();
+    });
+
+    test('clears interval', () => {
+        const callback = jest.fn();
+
+        const { result } = setup(callback, 100);
+        expect(clearInterval).toHaveBeenCalledTimes(0);
+
+        expect(callback).not.toBeCalled();
+
+        result.current();
+
+        jest.advanceTimersByTime(150);
+
+        expect(callback).toHaveBeenCalledTimes(0);
+        expect(clearInterval).toHaveBeenCalledTimes(1);
     });
 });
