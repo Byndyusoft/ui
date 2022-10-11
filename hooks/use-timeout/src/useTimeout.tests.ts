389c2a9241643dcd4064c91b9e1dd6e540b0ddac
Eugene Abrosimov <sadcitizen@yandex.ru>
2022-10-11 17:40:56 +0500

    feat(use-timeout): add tests

--- hooks/use-timeout/src/useTimeout.tests.ts
+++ hooks/use-timeout/src/useTimeout.tests.ts
@@ -1,8 +1,35 @@
 import { renderHook } from '@testing-library/react-hooks';
 import useTimeout from './useTimeout';
 
+const setup = (callback: () => void, delay: number | null) => renderHook(() => useTimeout(callback, delay));
+
 describe('hooks/useTimeout', () => {
-    test('to be true', () => {
-        expect(true).toBeTruthy();
+    beforeAll(() => {
+        jest.useFakeTimers();
+        jest.spyOn(global, 'clearTimeout');
+    });
+
+    test('calls callback', () => {
+        const callback = jest.fn();
+
+        setup(callback, 100);
+
+        expect(callback).not.toBeCalled();
+
+        jest.advanceTimersByTime(150);
+
+        expect(callback).toHaveBeenCalledTimes(1);
+    });
+
+    test('does not call callback if delay is null', () => {
+        const callback = jest.fn();
+
+        setup(callback, null);
+
+        expect(callback).not.toBeCalled();
+
+        jest.advanceTimersByTime(150);
+
+        expect(callback).not.toBeCalled();
     });
 });
--- hooks/use-timeout/src/useTimeout.ts
+++ hooks/use-timeout/src/useTimeout.ts
@@ -1,18 +1,16 @@
 import { useEffect } from 'react';
 import useLatestRef from '@byndyusoft-ui/use-latest-ref';
 
-export default function useTimeout(callback: () => void, delay = 0): void {
+export default function useTimeout(callback: () => void, delay: number | null): void {
     const savedCallback = useLatestRef(callback);
 
     useEffect(() => {
-        if (delay === 0) {
+        if (delay === null) {
             return undefined;
         }
 
         const timeoutId = setTimeout(() => savedCallback.current(), delay);
 
-        return () => {
-            clearTimeout(timeoutId);
-        };
-    }, [delay, savedCallback]);
+        return () => clearTimeout(timeoutId);
+    }, [delay]);
 }
