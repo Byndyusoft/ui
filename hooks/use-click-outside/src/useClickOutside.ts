ea6fc552e5c998b632dd311094e7cc4e36cb056a
Viktor Smorodin <smorodin@byndyusoft.com>
2022-11-07 23:21:17 +0500

    feat: [feature/useClickOutside] update useClickOutside perfomance

--- hooks/use-click-outside/src/useClickOutside.ts
+++ hooks/use-click-outside/src/useClickOutside.ts
@@ -1,17 +1,17 @@
-import { useCallback, MutableRefObject } from 'react';
-// import useEventListener from '@byndyusoft-ui/use-event-listener';
-import useEventListener from '../../use-event-listener/src';
+import {useCallback, MutableRefObject, useMemo} from 'react';
+import useEventListener from '@byndyusoft-ui/use-event-listener';
 
 export default function useClickOutside<T extends HTMLElement = HTMLElement>(
     refs: Array<MutableRefObject<T>>,
     handler: () => void
 ): void {
+    const memoizedRefs = useMemo(() => [...refs], [refs]);
     const internalHandler = useCallback((e: Event): void => {
-        const refWithEvent = refs.find(ref => ref.current.contains(e.target as Node));
+        const refWithEvent = memoizedRefs.find(ref => ref.current.contains(e.target as Node));
         if (!refWithEvent) {
             handler();
         }
-    }, [refs, handler]);
+    }, [memoizedRefs, handler]);
 
     useEventListener('click', internalHandler);
 }
