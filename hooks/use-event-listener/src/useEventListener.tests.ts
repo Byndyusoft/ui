4229b862c5019590d8866cebedb4b8f8d0e2bd78
Viktor Smorodin <smorodin@byndyusoft.com>
2022-11-23 23:12:06 +0500

    feat: [fature/useEventListener] try to add tests

--- hooks/use-event-listener/src/useEventListener.tests.ts
+++ hooks/use-event-listener/src/useEventListener.tests.ts
@@ -11,5 +11,37 @@ describe('hooks/useEventListener', () => {
         /* do anything */
     });
 
-    it('needs tests');
+    test('import useEventListener from "@byndyusoft/ui/use-event-listener"', () => {
+        expect(typeof useEventListener).toBe('function');
+    });
+
+    // test.skip('you pass an `eventType`, `handler`, and a `target`', async () => {
+    //     const handler = jest.fn();
+    //     const addEventListenerSpy = jest.spyOn(mockElement, 'addEventListener');
+    //
+    //     const { waitForNextUpdate } = renderHook(() =>
+    //         useEventListener('click', handler)
+    //     );
+    //
+    //     await waitForNextUpdate;
+    //     expect(addEventListenerSpy).toBeCalled();
+    //
+    //     mockElement.dispatchEvent(mouseMoveEvent);
+    //     expect(handler).toBeCalledWith(mouseMoveEvent);
+    //
+    //     addEventListenerSpy.mockRestore();
+    // });
+
+    test('"target" is optional (defaults to "document")', () => {
+        const handler = jest.fn();
+        const addEventListenerSpy = jest.spyOn(document, 'addEventListener');
+
+        setup('click', handler);
+
+        expect(addEventListenerSpy).toBeCalled();
+
+        addEventListenerSpy.mockRestore();
+    });
+
+    it.skip('needs tests');
 });
