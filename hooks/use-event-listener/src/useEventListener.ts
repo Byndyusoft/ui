c86d93b2defa0fb43e80bb03eecffd3fac681752
Eugene Abrosimov <sadcitizen@yandex.ru>
2022-11-26 18:27:00 +0500

    feat(useEventListener): update implementation

--- hooks/use-event-listener/src/useEventListener.ts
+++ hooks/use-event-listener/src/useEventListener.ts
@@ -1,22 +1,53 @@
 import { useEffect } from 'react';
 import useLatestRef from '@byndyusoft-ui/use-latest-ref';
 
-export default function useEventListener(
-    eventType: keyof GlobalEventHandlersEventMap,
-    handler: (e: Event) => void,
-    target: GlobalEventHandlers = document
-) {
+export function useEventListener<K extends keyof HTMLElementEventMap, T extends HTMLElement = HTMLDivElement>(
+    eventName: K,
+    handler: (this: T, event: HTMLElementEventMap[K]) => void,
+    target: T,
+    options?: boolean | AddEventListenerOptions
+): void;
+
+export function useEventListener<K extends keyof DocumentEventMap>(
+    eventName: K,
+    handler: (this: Document, event: DocumentEventMap[K]) => void,
+    target: Document,
+    options?: boolean | AddEventListenerOptions
+): void;
+
+export function useEventListener<K extends keyof WindowEventMap>(
+    eventName: K,
+    handler: (this: Window, event: WindowEventMap[K]) => void,
+    target: Window,
+    options?: boolean | AddEventListenerOptions
+): void;
+
+export default function useEventListener<
+    KH extends keyof HTMLElementEventMap,
+    KD extends keyof DocumentEventMap,
+    KW extends keyof WindowEventMap
+>(
+    eventName: KD | KH | KW | string,
+    handler: (
+        this: typeof target,
+        event: HTMLElementEventMap[KH] | DocumentEventMap[KD] | WindowEventMap[KW] | Event
+    ) => void,
+    target: Window | Document | HTMLElement = window,
+    options?: boolean | AddEventListenerOptions
+): void {
     const handlerRef = useLatestRef(handler);
 
     useEffect(() => {
-        function internalHandler(e: Event) {
-            return handlerRef.current(e);
+        if (!target.addEventListener) {
+            return;
         }
 
-        target.addEventListener(eventType, internalHandler);
+        const listener: typeof handler = event => handlerRef.current.call(target, event);
+
+        target.addEventListener(eventName, listener, options);
 
         return () => {
-            target.removeEventListener(eventType, internalHandler);
+            target.removeEventListener(eventName, listener, options);
         };
-    }, [eventType, target]);
+    }, [eventName, target, options]);
 }
