d1399a1ed45c9b8fa0299a56178ebf23d9e31cc3
Viktor Smorodin <smorodin@byndyusoft.com>
2023-01-25 16:24:24 +0500

    feat: [useClickOutside] remove useMemo

--- hooks/use-click-outside/src/useClickOutside.ts
+++ hooks/use-click-outside/src/useClickOutside.ts
@@ -1,17 +1,17 @@
-import { useCallback, MutableRefObject, useMemo } from 'react';
+import { useCallback, MutableRefObject } from 'react';
 import useEventListener from '@byndyusoft-ui/use-event-listener';
 
 export default function useClickOutside<T extends HTMLElement>(
     handler: () => void,
     ...refs: MutableRefObject<T | null>[]
 ): void {
-    const memoizedRefs = useMemo(() => [...refs], [refs]);
+    const stringifiedRefs = JSON.stringify(refs);
     const internalHandler = useCallback((e: Event): void => {
-        const refWithEvent = memoizedRefs.find(ref => ref.current !== null && ref.current.contains(e.target as Node));
+        const refWithEvent = refs.find(ref => ref.current !== null && ref.current.contains(e.target as Node));
         if (!refWithEvent) {
             handler();
         }
-    }, [memoizedRefs, handler]);
+    }, [stringifiedRefs, handler]);
 
     useEventListener('click', internalHandler, document);
 }
