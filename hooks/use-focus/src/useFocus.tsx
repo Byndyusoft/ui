6f56c7f666c20680336b571eaa019b8650841b19
Maksim Tetslav <maks.tetslav@yandex.ru>
2023-03-04 04:24:07 +0700

    feat: implement useFocus hook

---
+++ hooks/use-focus/src/useFocus.tsx
@@ -0,0 +1,21 @@
+import React, { RefObject, useEffect, useState } from 'react';
+import useEventListener from '@byndyusoft-ui/use-event-listener';
+
+export function useFocus<T extends HTMLElement = HTMLElement>(elementRef: RefObject<T>, defaultState = false): boolean {
+    const [isFocused, setIsFocused] = useState(defaultState);
+
+    useEffect(() => {
+        if (elementRef.current && defaultState) {
+            elementRef.current.focus();
+        }
+    }, []);
+
+    const onFocus = () => setIsFocused(true);
+
+    const onBlur = () => setIsFocused(false);
+
+    useEventListener('focus', onFocus, elementRef);
+    useEventListener('blur', onBlur, elementRef);
+
+    return isFocused;
+}
