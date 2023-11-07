83746fb3c3d41f3284676532f9954ea28b526107
Степан Карташев <kartashev@byndyusoft.com>
2023-11-07 15:40:36 +0500

    feat: update hygen component template

--- .templates/create/component/prompt.js
+++ .templates/create/component/prompt.js
@@ -5,6 +5,26 @@ module.exports = [
         message: 'Provide new component name.'
     },
     {
+        type: 'input',
+        name: 'folderName',
+        message: 'Provide new component folder name (kebab-case).'
+    },
+    {
+        type: 'input',
+        name: 'authorName',
+        message: 'Provide your surname and name.'
+    },
+    {
+        type: 'input',
+        name: 'authorEmail',
+        message: 'Provide your email address.'
+    },
+    {
+        type: 'input',
+        name: 'packageKeywords',
+        message: 'Provide keywords specialized for this component (divide them by spaces) (may be skipped).'
+    },
+    {
         type: 'confirm',
         name: 'hasStories',
         initial: 'Y',
