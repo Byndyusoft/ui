3d8f3901de7d870862d9b677b1bc7fa3ab8555c6
Eugene Abrosimov <sadcitizen@yandex.ru>
2024-01-14 14:35:44 +0500

    feat(useClickOutside): fix ref

--- hooks/use-click-outside/src/useClickOutside.ts
+++ hooks/use-click-outside/src/useClickOutside.ts
@@ -1,25 +1,29 @@
-import { useCallback, MutableRefObject } from 'react';
+import { useCallback, MutableRefObject, useRef } from 'react';
 import useEventListener from '@byndyusoft-ui/use-event-listener';
 
 export default function useClickOutside<T extends HTMLElement>(
     handler: () => void,
     ...refs: Array<MutableRefObject<T | null> | HTMLElement | null>
 ): void {
+    const documentRef = useRef(document);
     const stringifiedRefs = JSON.stringify(refs);
-    const internalHandler = useCallback((e: Event): void => {
-        const refWithEvent = refs.find(ref => {
-            if (ref === null) {
-                return false;
+    const internalHandler = useCallback(
+        (e: Event): void => {
+            const refWithEvent = refs.find(ref => {
+                if (ref === null) {
+                    return false;
+                }
+                if (ref instanceof HTMLElement) {
+                    return ref.contains(e.target as Node);
+                }
+                return ref.current !== null && ref.current.contains(e.target as Node);
+            });
+            if (!refWithEvent) {
+                handler();
             }
-            if (ref instanceof HTMLElement) {
-                return ref.contains(e.target as Node);
-            }
-            return ref.current !== null && ref.current.contains(e.target as Node)
-        });
-        if (!refWithEvent) {
-            handler();
-        }
-    }, [stringifiedRefs, handler]);
+        },
+        [stringifiedRefs, handler]
+    );
 
-    useEventListener('click', internalHandler, document);
+    useEventListener('click', internalHandler, documentRef);
 }
