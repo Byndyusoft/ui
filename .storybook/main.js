d59bdc98d43bd8f742e7b9309a007c9465570123
Nastya Vasenina <vasenina@byndyusoft.com>
2024-01-17 00:12:18 +0300

    feat: update version

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,14 +1,30 @@
+import { dirname, join } from "path";
 module.exports = {
     stories: ['../@(components|hooks)/**/src/**/*.stories.@(tsx|mdx)'],
+
     addons: [
-        '@storybook/addon-links',
-        '@storybook/addon-essentials',
-        '@storybook/addon-a11y',
+        getAbsolutePath("@storybook/addon-links"),
+        getAbsolutePath("@storybook/addon-essentials"),
+        getAbsolutePath("@storybook/addon-a11y"),
         {
             name: '@storybook/addon-docs',
             options: {
                 transcludeMarkdown: true
             }
-        }
-    ]
+        },
+        getAbsolutePath("@storybook/addon-mdx-gfm")
+    ],
+
+    framework: {
+        name: getAbsolutePath("@storybook/react-webpack5"),
+        options: {}
+    },
+
+    docs: {
+        autodocs: true
+    }
 };
+
+function getAbsolutePath(value) {
+    return dirname(require.resolve(join(value, "package.json")));
+}
