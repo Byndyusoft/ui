c6bf0b4a0260aff49f09286c2ec85f71c48f0098
Eugene Abrosimov <sadcitizen@yandex.ru>
2024-01-17 17:11:49 +0500

    feat(withClassNames): add implementation

---
+++ hocs/with-class-names/src/withClassNames.tsx
@@ -0,0 +1,7 @@
+import React, { ComponentType, PropsWithChildren } from 'react';
+import { IClassNames } from './withClassNames.types';
+
+export default function withClassNames<P, CN>(Component: ComponentType<P & IClassNames<CN>>, classNames: CN) {
+    // eslint-disable-next-line react/display-name,@typescript-eslint/explicit-module-boundary-types
+    return (props: PropsWithChildren<P>) => <Component {...props} classNames={classNames} />;
+}
