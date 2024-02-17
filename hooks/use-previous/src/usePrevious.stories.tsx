bff68c6fdac384ce1d72ebf5bfc4b67c27c7a636
Nastya Vasenina <vasenina@byndyusoft.com>
2024-02-17 20:43:18 +0300

    fix: fix stories

---
+++ hooks/use-previous/src/usePrevious.stories.tsx
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+import usePrevious from './usePrevious';
+import type { Meta, StoryObj } from '@storybook/react';
+import './usePrevious.stories.css';
+
+type Story = StoryObj<typeof Template>;
+
+const Template = () => {
+    const [counter, setCounter] = useState(0);
+    const res = usePrevious(counter);
+
+    return (
+        <>
+            <button type="button" onClick={() => setCounter(counter + 1)}>
+                Update value
+            </button>
+            <div className="container">
+                <span>Previous value: {res}</span>
+                <span>Current value: {counter}</span>
+            </div>
+        </>
+    );
+};
+
+export const HookStory: Story = {
+    decorators: [() => <Template />]
+};
+
+const meta: Meta<typeof Template> = {
+    title: 'hooks/usePrevious'
+    // component: Template
+};
+
+export default meta;
