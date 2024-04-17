6f85519f5f67d1049641bb665fa070d03a660cc3
Nastya Vasenina <vasenina@byndyusoft.com>
2024-04-17 16:00:09 +0300

    fix: fix tests

--- hooks/use-debounced-callback/src/useDebouncedCallback.tests.tsx
+++ hooks/use-debounced-callback/src/useDebouncedCallback.tests.tsx
@@ -1,40 +1,25 @@
-import React from 'react';
-import { render, screen, waitFor } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
+import { waitFor } from '@testing-library/react';
+import { renderHook, act } from '@testing-library/react-hooks';
 import useDebouncedCallback from './useDebouncedCallback';
 
 const oldValue = 'old value';
 const newValue = 'new value';
 const timeout = 2000;
-const debouncedChangeLabel = 'debounced change label';
-
-const Setup = (): JSX.Element => {
-    let value = oldValue;
-    const handleChangeValue = () => (value = newValue);
-    const setDebounceValue = useDebouncedCallback(handleChangeValue, timeout);
-
-    return (
-        <div className="container">
-            <span>
-                Value: <span>{value}</span>
-            </span>
-            <button aria-label={debouncedChangeLabel} onClick={setDebounceValue}>
-                debounced change
-            </button>
-        </div>
-    );
-};
 
 describe('hooks/useDebouncedCallback', () => {
-    test('useDebouncedCallback works', async () => {
-        render(<Setup />);
-        expect(screen.findByText(oldValue)).toBeInTheDocument();
-        const debouncedChangeButton = await screen.getByLabelText(debouncedChangeLabel);
-        userEvent.click(debouncedChangeButton);
-        expect(screen.findByText(oldValue)).toBeInTheDocument();
-        await waitFor(
+    test('useDebouncedCallback works', () => {
+        let value = oldValue;
+        const handle = () => (value = newValue);
+        const { result } = renderHook(() => useDebouncedCallback(handle, timeout));
+        const setDebounceValue = result.current;
+        act(() => {
+            setDebounceValue();
+        });
+
+        expect(value).toEqual(oldValue);
+        waitFor(
             () => {
-                expect(screen.findByText(newValue)).toBeInTheDocument();
+                expect(value).toEqual(newValue);
             },
             { timeout: 2500 }
         );
--- hooks/use-debounced-value/src/useDebouncedValue.tests.tsx
+++ hooks/use-debounced-value/src/useDebouncedValue.tests.tsx
@@ -1,36 +1,25 @@
-import React from 'react';
-import { render, screen, waitFor } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
+import { waitFor } from '@testing-library/react';
+import { renderHook, act } from '@testing-library/react-hooks';
 import useDebouncedValue from './useDebouncedValue';
 
-const debouncedValueLabel = 'debounced value';
-const inputValue = '1111';
-
-const Setup = (): JSX.Element => {
-    const [debouncedValue, setDebouncedValue] = useDebouncedValue('', 2000);
-
-    return (
-        <div className="container">
-            <span>
-                Debounced value: <span aria-label={debouncedValueLabel}>{debouncedValue}</span>
-            </span>
-            <input onChange={e => setDebouncedValue(e.target.value)} />
-        </div>
-    );
-};
+const oldValue = 'old value';
+const newValue = 'new value';
+const timeout = 2000;
 
 describe('hooks/useDebouncedValue', () => {
-    test('useDebouncedValue works', async () => {
-        render(<Setup />);
-
-        const input = await screen.findByRole('textbox');
-        userEvent.type(input, inputValue);
-
-        await waitFor(
+    test('useDebouncedValue works', () => {
+        const { result } = renderHook(() => useDebouncedValue(oldValue, timeout));
+        const [debouncedValue, setDebouncedValue] = result.current;
+        expect(debouncedValue).toEqual(oldValue);
+        act(() => {
+            setDebouncedValue(newValue);
+        });
+        expect(debouncedValue).toEqual(oldValue);
+        waitFor(
             () => {
-                expect(screen.getByLabelText(debouncedValueLabel)).toHaveTextContent(inputValue);
+                expect(debouncedValue).toEqual(newValue);
             },
-            { timeout: 2100 }
+            { timeout: 2500 }
         );
     });
 });
