a64510c8753d6ac5b4b88beae136a2c1639a26f0
Dmitrii Maletskii <mltsk94@gmail.com>
2024-05-07 13:25:02 +0300

    feat: rollup-plugin-styles

--- rollup.base.config.js
+++ rollup.base.config.js
@@ -1,6 +1,7 @@
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import peerDeps from 'rollup-plugin-peer-deps-external';
+import styles from 'rollup-plugin-styles';
 
 export default {
     output: {
@@ -8,7 +9,8 @@ export default {
         format: 'cjs',
         dir: 'dist',
         preserveModules: true,
-        preserveModulesRoot: 'src'
+        preserveModulesRoot: 'src',
+        assetFileNames: '[name]-[hash][extname]'
     },
-    plugins: [peerDeps(), resolve(), commonjs()]
+    plugins: [peerDeps(), resolve(), commonjs(), styles()]
 };
