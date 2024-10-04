119850cb2d3ea2ccfbaa1392ea0e366759448645
Gleb Fomin <gleb.fom28@gmail.com>
2024-10-04 22:34:07 +0500

    feat(types): update Callback, add IsTuple

--- packages/types/src/Callback.ts
+++ packages/types/src/Callback.ts
@@ -1,1 +1,11 @@
-export type Callback<V, R = void> = V extends unknown[] ? (...args: V) => R : (arg: V) => R;
+type IsTuple<T> = T extends readonly unknown[]
+  ? (T['length'] extends number
+    ? (number extends T['length'] ? false : true)
+    : false)
+  : false;
+
+
+
+type Callback<V, R = void> = IsTuple<V> extends true
+  ? (...args: V extends unknown[] ? V : []) => R
+  : (arg: V) => R;
