2c0e3de5a10760c6b1751e76cb30f2f4a08e7b2d
Dmitrii Maletskii <mltsk94@gmail.com>
2024-10-29 19:33:48 +0300

    Revert "WIP"
    This reverts commit 29096502b09c0ec11047934000ad71d92c5eb3b2.

--- components/Overlay/src/Overlay.tsx
+++ components/Overlay/src/Overlay.tsx
@@ -1,12 +1,7 @@
 import React, { FC, useEffect } from 'react';
-import { css } from '@linaria/core';
 import cn from 'classnames';
 import { IOverlayProps } from './Overlay.types';
-// import './Overlay.css';
-
-export const overlayCss = css`
-    color: red;
-`;
+import './Overlay.css';
 
 const Overlay: FC<IOverlayProps> = ({
     children,
@@ -27,10 +22,22 @@ const Overlay: FC<IOverlayProps> = ({
         }
     }, [isVisible]);
 
+    const hexToRgba = (hex: string, opacity?: number) => {
+        let r = parseInt(hex.substring(1, 3), 16);
+        let g = parseInt(hex.substring(3, 5), 16);
+        let b = parseInt(hex.substring(5, 7), 16);
+        return `rgba(${r},${g},${b},${opacity || 0.6})`;
+    };
+
     return (
         <>
             <div
-                className={cn('bs-overlay', overlayCss, isVisible && 'is-visible', center && 'center')}
+                className={cn('bs-overlay', isVisible && 'is-visible', center && 'center')}
+                style={{
+                    zIndex: zIndex ? zIndex : 0,
+                    backgroundColor: color ? hexToRgba(color, backgroundOpacity) : '',
+                    backdropFilter: blur ? `blur(${blur}px)` : ''
+                }}
                 role="presentation"
                 tabIndex={-1}
                 {...props}
