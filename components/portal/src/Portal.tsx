f317766367fe6b98401c0300127296fc2d9d6072
ancientbag <pleasenonaga@gmail.com>
2024-10-31 18:43:10 +0500

    feat: portal with targetElement property

--- components/portal/src/Portal.tsx
+++ components/portal/src/Portal.tsx
@@ -3,7 +3,11 @@ import { createPortal } from 'react-dom';
 import usePortal from './usePortal';
 import { IPortalProps } from './Portal.types';
 
-const Portal: FC<PropsWithChildren<IPortalProps>> = ({ children, id }) => {
+const Portal: FC<PropsWithChildren<IPortalProps>> = ({ children, id, targetElement }) => {
+    if (targetElement) {
+        return createPortal(children, targetElement);
+    }
+
     const { container } = usePortal({ id });
 
     return createPortal(children, container);
