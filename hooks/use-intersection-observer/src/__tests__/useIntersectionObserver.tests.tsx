9a0f4b6f7586722dc54f06176c7cb85837381cda
Gleb Fomin <fomin@byndyusoft.com>
2024-11-11 13:18:04 +0500

    chore(hooks/useIntersectionObserver): removed unused imports

--- hooks/use-intersection-observer/src/__tests__/useIntersectionObserver.tests.tsx
+++ hooks/use-intersection-observer/src/__tests__/useIntersectionObserver.tests.tsx
@@ -1,5 +1,5 @@
-import { render, screen } from '@testing-library/react';
-import React, { lazy, useCallback } from 'react';
+import { render } from '@testing-library/react';
+import React from 'react';
 import { IUseIntersectionObserverOptions } from '../useIntersectionObserver.types';
 import {
     intersectionMockInstance,
