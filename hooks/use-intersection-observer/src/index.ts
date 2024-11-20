300d006b3d2b6790d855cdce840e92fbfc5ae64f
Gleb Fomin <fomin@byndyusoft.com>
2024-11-20 07:00:19 +0500

    chore(useIntersectionObserver): import tests helpers

--- hooks/use-intersection-observer/src/index.ts
+++ hooks/use-intersection-observer/src/index.ts
@@ -1,6 +1,12 @@
 import useIntersectionObserver from './useIntersectionObserver';
 
 export type { IUseIntersectionObserverOptions, IUseIntersectionObserverReturn } from './useIntersectionObserver.types';
-export * from './__tests__/useIntersectionObserver.mocks';
+export {
+    intersectionMockInstance,
+    mockIsIntersecting,
+    setupIntersectionMocking,
+    mockAllIsIntersecting,
+    resetIntersectionMocking
+} from './__tests__/useIntersectionObserver.mocks';
 export { useIntersectionObserver };
 export default useIntersectionObserver;
