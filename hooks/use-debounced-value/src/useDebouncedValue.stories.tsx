157d3e4ff5127bab4f9a1fe4c62ceb30dbfb9e1e
Viktor Smorodin <smorodin@byndyusoft.com>
2024-12-05 12:33:04 +0300

    feat(useDebounce): поправил стили

--- hooks/use-debounced-value/src/useDebouncedValue.stories.tsx
+++ hooks/use-debounced-value/src/useDebouncedValue.stories.tsx
@@ -1,18 +1,18 @@
 import React, { useState } from 'react';
 import { StoryObj } from '@storybook/react';
 import useDebouncedValue from './useDebouncedValue';
-import styles from './useDebouncedValue.stories.module.css';
+import './useDebouncedValue.stories.css';
 
 const DebouncedInput = () => {
     const [delay, setDelay] = useState(1000);
     const [debouncedValue, setDebouncedValue] = useDebouncedValue('', delay);
 
     return (
-        <div className={styles.container}>
-            <div className={styles.block}>
-                <span className={styles.title}>Delay:</span>
+        <div className="use-debounced-value__container">
+            <div className="use-debounced-value__block">
+                <span className="use-debounced-value__title">Delay:</span>
                 <button
-                    className={styles.button}
+                    className="use-debounced-value__button"
                     type="button"
                     disabled={delay <= 0}
                     onClick={() => setDelay(delay - 100)}
@@ -21,21 +21,21 @@ const DebouncedInput = () => {
                 <span>{`${delay} ms`}</span>
 
                 <button
-                    className={styles.button}
+                    className="use-debounced-value__button"
                     type="button"
                     onClick={() => setDelay(delay + 100)}
                 >+</button>
             </div>
 
-            <div className={styles.block}>
-                <span className={styles.title}>Type anything:</span>
-                <input className={styles.input} onChange={e => setDebouncedValue(e.target.value)} />
+            <div className="use-debounced-value__block">
+                <span className="use-debounced-value__title">Type anything:</span>
+                <input className="use-debounced-value__input" onChange={e => setDebouncedValue(e.target.value)} />
             </div>
 
-            <div className={styles.divider} />
+            <div className="use-debounced-value__divider" />
 
-            <div className={styles.block}>
-                <span className={styles.title}>Debounced result:</span>
+            <div className="use-debounced-value__block">
+                <span className="use-debounced-value__title">Debounced result:</span>
                 <span>{debouncedValue}</span>
             </div>
         </div>
