328d7714cd33aede9d8da5a41775108018a7c1e5
Andrey Zhukov <Zhukov290791@gmail.com>
2025-01-23 00:24:48 +0500

    feat(useDebounce): improve type for `useDebouncedValue`

--- hooks/use-debounced-value/src/useDebouncedValue.ts
+++ hooks/use-debounced-value/src/useDebouncedValue.ts
@@ -1,9 +1,10 @@
-import { useState, useMemo } from 'react';
+import { useMemo, useState } from 'react';
+import { InitialState } from '@byndyusoft-ui/types';
 import useDebouncedCallback from '@byndyusoft-ui/use-debounced-callback';
 
 type THookReturn<T> = [T, (arg: T) => void];
 
-export default function useDebouncedValue<T>(value: T, delay = 300): THookReturn<T> {
+export default function useDebouncedValue<T>(value: InitialState<T>, delay = 300): THookReturn<T> {
     const [debouncedValue, setValue] = useState(value);
 
     const setDebouncedValue = useDebouncedCallback(setValue, delay);
