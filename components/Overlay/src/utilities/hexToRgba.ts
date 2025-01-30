90247973bacd9c74c4016f24593c3bfb91cdcb41
Dmitrii Maletskii <mltsk94@gmail.com>
2025-01-30 15:35:20 +0300

    fix(feature/overlay): hexToRgba

--- components/Overlay/src/utilities/hexToRgba.ts
+++ components/Overlay/src/utilities/hexToRgba.ts
@@ -1,8 +1,8 @@
-const hexToRgba = (hex: string, opacity?: number): string => {
+const hexToRgba = (hex: string, opacity: number): string => {
     const r = parseInt(hex.substring(1, 3), 16);
     const g = parseInt(hex.substring(3, 5), 16);
     const b = parseInt(hex.substring(5, 7), 16);
-    return `rgba(${r},${g},${b},${opacity ?? 0.6})`;
+    return `rgba(${r},${g},${b},${opacity})`;
 };
 
 export { hexToRgba };
