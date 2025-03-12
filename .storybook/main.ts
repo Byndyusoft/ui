cfaf36a7b2fdb54979102f2afe7cdc5dfc777238
Nikolai Kuznetsov <nick-chel@yandex.ru>
2025-03-12 14:57:44 +0500

    storybook webpack => Vite

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,32 +1,35 @@
 import { dirname, join } from 'path';
-module.exports = {
+import type {StorybookConfig} from '@storybook/react-vite';
+
+function getAbsolutePath(value: string): string {
+    return dirname(require.resolve(join(value, 'package.json')));
+}
+
+const config:StorybookConfig = {
     stories: [
-        '../components/**/src/**/*.stories.@(js|jsx|ts|tsx)',
-        '../components/**/src/**/*.docs.mdx',
-        '../hooks/**/src/**/*.stories.@(js|jsx|ts|tsx)',
-        '../hooks/**/src/**/*.docs.mdx',
+        '../@(components|hooks)/**/src/**/*.stories.@(js|jsx|ts|tsx)',
+        '../@(components|hooks)/**/src/**/*.docs.mdx',
         '../styles/**/*.stories.@(js|jsx|ts|tsx)',
         '../styles/**/*.docs.mdx'
     ],
 
     addons: [
-        getAbsolutePath('@storybook/addon-links'),
         getAbsolutePath('@storybook/addon-essentials'),
         getAbsolutePath('@storybook/addon-a11y'),
-        getAbsolutePath('@storybook/addon-docs'),
-        getAbsolutePath('@storybook/addon-mdx-gfm'),
-        getAbsolutePath('@newhighsco/storybook-addon-svgr'),
-        getAbsolutePath("@storybook/addon-mdx-gfm"),
-        // getAbsolutePath("@storybook/addon-webpack5-compiler-swc")
     ],
 
-    framework: '@storybook/react-vite',
+    framework: getAbsolutePath('@storybook/react-vite'),
 
     docs: {
         defaultName: 'Documentation'
-    }
+    },
+
+    typescript: {
+        // Overrides the default Typescript configuration to allow multi-package components to be documented via Autodocs.
+        reactDocgen: 'react-docgen-typescript',
+        check: false,
+    },
 };
 
-function getAbsolutePath(value: string): string {
-    return dirname(require.resolve(join(value, 'package.json')));
-}
+export default config;
+
