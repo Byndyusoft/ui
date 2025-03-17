8d7b46976f9aa3c1c748b4f4e43efe29a8718fc5
Nikolai Kuznetsov <nick-chel@yandex.ru>
2025-03-18 00:43:16 +0500

    correct style file extension

--- components/portal/src/Portal.stories.tsx
+++ components/portal/src/Portal.stories.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import cn from 'classnames';
 import { StoryObj } from '@storybook/react';
 import Portal from './Portal';
-import './PortalStory.module.scss';
+import './PortalStory.module.css';
 
 const Template = (): JSX.Element => (
     <div className="portalStory">
