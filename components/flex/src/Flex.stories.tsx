3ec273cfc9bd5444346e1bb24e25abca47b9df8b
Nikolai Kuznetsov <nick-chel@yandex.ru>
2025-03-18 00:43:22 +0500

    Flex component

--- components/flex/src/Flex.stories.tsx
+++ components/flex/src/Flex.stories.tsx
@@ -1,16 +1,35 @@
 import React from 'react';
-import { StoryObj } from '@storybook/react';
+import { Meta, StoryObj } from '@storybook/react';
 import { IFlexProps } from './Flex.types';
 import Flex from './Flex';
+import styles from './FlexStories.module.css';
+import cn from "classnames";
 
-const Template = (args: IFlexProps): JSX.Element => <Flex {...args}>1231</Flex>;
-
-export const FlexStory: StoryObj<typeof Template> = {
-    name: 'Story name',
-    render: Template,
-    args: {}
+const meta: Meta<typeof Flex> = {
+    title: 'components/Flex',
+    component: Flex
 };
 
-export default {
-    title: 'components/Flex'
+export default meta;
+
+const FlexContent = () => {
+    return (
+        <>
+            <div className={styles.flexContent} />
+            <div className={cn(styles.flexContent, styles.flexContentBig)} />
+            <div className={styles.flexContent} />
+            <div className={cn(styles.flexContent, styles.flexContentMid)} />
+        </>
+    );
+};
+
+const Template = (args: IFlexProps): JSX.Element => (
+    <Flex {...args} className={styles.templateContainer}>
+        <FlexContent />
+    </Flex>
+);
+
+export const Default: StoryObj<typeof Template> = {
+    render: Template,
+    args: {}
 };
