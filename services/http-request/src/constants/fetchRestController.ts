57b93f255489b6fe401cef698fdff1ff07b2fca4
ancientbag <pleasenonaga@gmail.com>
2025-05-12 16:49:31 +0500

    feat: workaround for Headers collection for fetch

--- services/http-request/src/constants/fetchRestController.ts
+++ services/http-request/src/constants/fetchRestController.ts
@@ -1,59 +1,74 @@
 import HttpRestController from '../restController';
 
 export class HttpRestControllerFetch extends HttpRestController {
+    public headers: Headers = new Headers();
+
     constructor() {
         super();
-        this.headers = new Headers();
     }
-
-    public headers: Headers;
 
     setHeader = (key: string, value: string | null): void => {
         if (value) {
-            this.headers.set(key, value);
+            this.headers.append(key, value);
         } else {
             this.headers.delete(key);
         }
     };
 
-    get = async <R>(url: string, headers?: Headers): Promise<R> => {
-        return fetch(url, { method: 'GET', headers: { ...this.headers, ...headers } }).then(r =>
-            r.json()
-        ) as Promise<R>;
+    private getCurrentHeaders() {
+        const currentHeaders: Record<string, string> = {};
+        this.headers.forEach((h, v) => {
+            currentHeaders[h] = v;
+        });
+
+        return currentHeaders;
+    }
+
+    private mergeCurrentHeaders(headers: Headers | undefined) {
+        const collectedHeaders: Record<string, string> = this.getCurrentHeaders();
+
+        if (headers) {
+            headers.forEach((h, v) => {
+                collectedHeaders[h] = v;
+            });
+        }
+
+        return collectedHeaders;
+    }
+
+    get = async <R = Response>(url: string, headers?: Headers): Promise<R> => {
+        return fetch(url, { method: 'GET', headers: this.mergeCurrentHeaders(headers) }) as Promise<R>;
     };
 
-    post = async <R>(url: string, body: object, headers?: Headers): Promise<R> => {
+    post = async <R = Response>(url: string, body: object, headers?: Headers): Promise<R> => {
         return fetch(url, {
             method: 'POST',
             body: JSON.stringify(body),
-            headers: {
-                ...this.headers,
-                ...headers
-            }
-        }).then(r => r.json()) as Promise<R>;
+            headers: this.mergeCurrentHeaders(headers)
+        }) as Promise<R>;
     };
 
-    patch = async <R>(url: string, body: object, headers?: Headers): Promise<R> => {
+    patch = async <R = Response>(url: string, body: object, headers?: Headers): Promise<R> => {
         return fetch(url, {
             method: 'PATCH',
             body: JSON.stringify(body),
-            headers: { ...this.headers, ...headers }
-        }).then(r => r.json()) as Promise<R>;
+            headers: this.mergeCurrentHeaders(headers)
+        }) as Promise<R>;
     };
 
-    put = async <R>(url: string, body: object, headers?: Headers): Promise<R> => {
+    put = async <R = Response>(url: string, body: object, headers?: Headers): Promise<R> => {
         return fetch(url, {
             method: 'PUT',
             body: JSON.stringify(body),
-            headers: { ...this.headers, ...headers }
-        }).then(r => r.json()) as Promise<R>;
+            headers: this.mergeCurrentHeaders(headers)
+        }) as Promise<R>;
     };
 
-    delete = async <R>(url: string, body: object = {}, headers?: Headers): Promise<R> => {
+    delete = async <R = Response>(url: string, body: object = {}, headers?: Headers): Promise<R> => {
         return fetch(url, {
             method: 'DELETE',
             body: JSON.stringify(body),
-            headers: { ...this.headers, ...headers }
-        }).then(r => r.json()) as Promise<R>;
+            headers: this.mergeCurrentHeaders(headers)
+        }) as Promise<R>;
     };
 }
