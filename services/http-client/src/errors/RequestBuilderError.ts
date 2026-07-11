8a81c589d5d953ecf82b3420d231dca78ad6578c
Eugene Abrosimov <sadcitizen@yandex.ru>
2026-07-11 22:46:57 +0500

    feat(http): add request builder error codes

--- services/http-client/src/errors/RequestBuilderError.ts
+++ services/http-client/src/errors/RequestBuilderError.ts
@@ -1,7 +1,13 @@
+import { REQUEST_BUILDER_ERROR_CODES } from '../constants';
+import { TRequestBuilderErrorCode } from '../types';
+
 export class RequestBuilderError extends Error {
-    constructor(message: string) {
+    public readonly code: TRequestBuilderErrorCode;
+
+    constructor(message: string, code: TRequestBuilderErrorCode = REQUEST_BUILDER_ERROR_CODES.INVALID_CONFIG) {
         super(message);
         this.name = this.constructor.name;
+        this.code = code;
 
         Object.setPrototypeOf(this, new.target.prototype);
     }
